ddb8c9dc9f802604165ecc8f3f3f3cfe6e71ba91
Keren <keren.berelson@pearson.com>
2018-04-20 12:36:37 +0100

    Changed database and setup more along lines of final program@

--- src/server/index.js
+++ src/server/index.js
@@ -6,32 +6,36 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(5000);
 
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'root1234',
-    database : 'express'
+    database : 'KM2 Project'
   });
   
   connection.connect()
-  
-  connection.query('SELECT * from user', function (err, rows, fields) {
-    if (err) throw err
-  
-    console.log('The users are: ', rows)
-  });
+  console.log('Connected...')
 
 // routes (get, post, etc) -> put into separate file
 app.post("/user", function(req, res) {
     const body = req.body;
 
-    connection.query("INSERT INTO user (username, password, firstname, lastname) VALUES ?",
-     [[[body.uname, body.pwd, body.fname, body.lname]]], function (error, results) {
+    connection.query("INSERT INTO Users (Role, Username, TempPassword, Name) VALUES ?",
+     [[[body.Role, body.Username, body.TempPassword, body.Name]]], function (error, results) {
         if (error) throw error;
     });
 
     res.send(body);
+});
+
+app.get("/getUsers", function(req, res) {
+    connection.query('SELECT * from Users', function (err, rows, fields) {
+        if (err) throw err
+
+        res.send(rows);
+    });
 });
 
 process.on('exit', function () {
