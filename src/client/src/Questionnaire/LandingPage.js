36a94afaefb31225efcbc0c115ddc2cbf861a613
Keren <keren.berelson@pearson.com>
2018-05-11 17:07:38 +0100

    Added text to landng page

--- src/client/src/Questionnaire/LandingPage.js
+++ src/client/src/Questionnaire/LandingPage.js
@@ -7,6 +7,25 @@ class LandingPage extends Component {
 
       return (
       <div className="landingPage">
+        <p>
+          To determine which team role best describes your personality 
+          and operating style, consider the statements in each of the 
+          sections below. Please note: there are no right or wrong 
+          answers, so take as much time as you need to consider each 
+          statement honestly.
+        </p>  
+        <p>
+          For each section distribute a total of 10 points among the 
+          statements which you think best describe your behaviour. The 
+          points may be distributed among several sentences and in 
+          extreme cases they might be spread across all statements or 
+          all ten points given to one statement only.
+        </p>
+        <p>  
+          Give the most points to the statement which best describes 
+          your behaviour and the least points (or no points) to the statements that are least accurate or do not apply.
+        </p>
+        
         <QuestionTable
           className = "landingPageTable"
           titleText = {exampleText.titleText}
