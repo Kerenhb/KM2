8a249c631f96dc5bfd25775276226d8db4953988
Keren <keren.berelson@pearson.com>
2018-06-01 15:27:59 +0100

    Client: Improved results page

--- TODO.md
+++ TODO.md
@@ -5,7 +5,6 @@ Basically more of a thin-slice agile development method.
 
 **Next:** Add in basic questionnaire section so you can answer each question one at a time and then submit your results (which saves it to the database).
 - Add a time-code of when you took the test (server side)
-- Improve results page to show the role descriptions and clearly identify your top 3 roles
 - Deal with warning about going from unControlled to controlled input
 - Attempt to make number input look better (increase the width to match the cell?)
 - Progress bar x/7?
--- src/client/src/Questionnaire/ResultsPage.js
+++ src/client/src/Questionnaire/ResultsPage.js
@@ -1,30 +1,74 @@
 import React, { Component } from 'react';
+import RoleDescriptions from './RoleDescriptions.json';
+import './Questionnaire.css';
 
 class ResultsPage extends Component {
   convertResultsToRoles = () => {
     const { results } = this.props;
     const roleKeys = ['IM', 'CO', 'SH', 'PL', 'RI', 'ME', 'TW', 'CF'];
-    const roles = [];
+    const parsedResults = [];
 
     results.map( (result, index) => {
-        roles.push({
+        parsedResults.push({
             role: roleKeys[index],
-            value: result,
+            percentage: Math.round(result / 0.7),
         });
     });
 
-    return roles.sort(function(a,b) { return b.value - a.value })
+    return parsedResults.sort(function(a,b) { return b.percentage - a.percentage })
+  };
+
+  constructRow = (role) => {
+      return (<tr>
+        <td rowSpan='2'>{RoleDescriptions[role].Type}</td>
+        <td>{RoleDescriptions[role].Features}</td>
+        <td>{RoleDescriptions[role].Strengths}</td>
+        <td>{RoleDescriptions[role].Weaknesses}</td>
+    </tr>)
+  };
+
+  constructDetailedRows = results => {
+    let rows = [];
+    for (let i = 0; i < 8; i++) {
+        rows.push(
+            <li key = {results[i].role}>
+                {RoleDescriptions[results[i].role].Type} - {results[i].percentage}%
+            </li>
+        )
+    }
+
+    return(<ul> {rows} </ul>)
   };
 
   render() {
-      const roles = this.convertResultsToRoles();
-      return (
-          <div>
-            {roles.map( ({ role, value }) => { return (
-                <p>{`You are ${Math.round(value/0.7)}% role ${role}`}</p>
-            )})}
-          </div>
-    );
+        const results = this.convertResultsToRoles();
+        const topRole = results[0].role;
+        const secondRole = results[1].role
+        const thirdRole = results[2].role;
+        return (
+            <div>
+                <p>Your top three roles are: {RoleDescriptions[topRole].Type}, {RoleDescriptions[secondRole].Type} and {RoleDescriptions[thirdRole].Type}.</p>
+                <table className='ResultsTable'>
+                    <thead><tr>
+                        <th>Type</th>
+                        <th>Typical Features</th>
+                        <th>Strengths</th>
+                        <th>Allowable Weakness</th>
+                    </tr></thead>
+                    <tbody>
+                        {this.constructRow(topRole)}
+                        <tr className='summary'><td colSpan='3'>{RoleDescriptions[topRole].Summary}</td></tr>
+                        {this.constructRow(secondRole)}
+                        <tr className='summary'><td colSpan='3'>{RoleDescriptions[secondRole].Summary}</td></tr>
+                        {this.constructRow(thirdRole)}
+                        <tr className='summary'><td colSpan='3'>{RoleDescriptions[thirdRole].Summary}</td></tr>
+                    </tbody>
+                </table>
+                <div>
+                    Here is the detailed (rounded) breakdown of your roles: {this.constructDetailedRows(results)}
+                </div>
+            </div>
+        );
   }
 }
 
