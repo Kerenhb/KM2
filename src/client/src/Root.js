1dbad6b861cf6ace46e3c2170c683d9db0992518
Keren <keren.berelson@pearson.com>
2018-06-19 13:46:26 +0100

    Added improved lock for Questionaire, still won't work with async call though

--- src/client/src/Root.js
+++ src/client/src/Root.js
@@ -1,10 +1,29 @@
-import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'; // remove non dom
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import React, { Component } from 'react';
 
 import Questionnaire from './Questionnaire/index';
 import Login from './Login';
 
 export default class Root extends Component {
+  privateRoute = (DestinationComponent) => {
+    const loggedIn = false;// await this.isAuthenticated();
+    let finalComponent = <Redirect to={{pathname: '/login'}}/>;
+    if(loggedIn) finalComponent = <DestinationComponent />;
+
+    return finalComponent;
+  }
+  
+isAuthenticated = async () => {
+    const response = await fetch('/verify', {
+        method: 'GET',
+        headers: { 'content-type': 'application/json' },
+        credentials: 'include'
+      })
+
+      const result = await response.json();
+      return result.loggedIn;
+};
+
     render() {
       return (
             <Router>
@@ -17,40 +36,10 @@ export default class Root extends Component {
                 <hr/>
 
                 <Route exact path="/login" component={Login}/>
-                <PrivateRoute path="/Questionnaire" component={Questionnaire}/>
+                <Route path="/Questionnaire" render={() => this.privateRoute(Questionnaire)}/>
                 <Route path="/error" render={() => <div>Error!</div>}/>
             </div>
             </Router>
       );
     }
-  };
-
-  const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route
-      {...rest}
-      render={props =>
-        isAuthenticated() ? ( //Doesn't work!
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: '/login',
-              state: { from: props.location }
-            }}
-          />
-        )
-      }
-      
-    />
-  );
-  
-async function isAuthenticated() {
-    const response = await fetch('/verify', {
-        method: 'GET',
-        headers: { 'content-type': 'application/json' },
-        credentials: 'include'
-      })
-
-      const result = await response.json();
-      return result.loggedIn;
-};  
+  };  
